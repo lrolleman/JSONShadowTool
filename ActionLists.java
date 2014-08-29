6dced3e6bd9513fdc1107096ee215a655b2cd605
Lyle Rolleman <lrollema@ualberta.ca>
2014-08-29 11:34:45 -0600

    v2.0, packaged jar

--- ActionLists.java
+++ ActionLists.java
@@ -1,139 +1,98 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
 
 public class ActionLists {
+	public static ArrayList<ArrayList<ArrayList<String>>> mlist;
+	public static ArrayList<ArrayList<ArrayList<String>>> alist;
+	private static String curtype = null;
 	
-	/*
-	 * Format:
-	 * index 0: {"JSON key", "multiplier"},
-	 * index 1+: {".json file name", multiplier override} where the override is optional
-	 * Be careful when typing these out, an error could cause the program to create a whole bunch of new folders where you don't want them
-	 * If specified correctly, all folders in the mod directory should be created for you. Replace the username in basedir 
-	 * and run it for a working example
-	 */
+	public static String srcdir;
+	public static String moddir;
 	
-	public static final boolean v1 = false;
-	public static final String srcdir = "C:\\Program Files (x86)\\Uber Entertainment\\Planetary Annihilation Launcher\\Planetary Annihilation\\PTE\\media\\pa";
-	public static final String moddir = "C:\\Users\\Lyle\\AppData\\Local\\Uber Entertainment\\Planetary Annihilation\\server_mods\\com.nixtempestas.antipopcorn\\pa";
-	
-	private static String[][] max_health_change = {
-		{"max_health", "2"},
-		{"air_scout.json"},
-		{"bomber.json"},
-		{"bomber_adv.json"},
-		{"fabrication_aircraft.json"},
-		{"fabrication_aircraft_adv.json"},
-		{"fighter.json"},
-		{"gunship.json"},
-		{"transport.json"},
-		{"aa_missile_vehicle.json"},
-		{"assault_bot.json"},
-		{"assault_bot_adv.json"},
-		{"bot_aa.json"},
-		{"bot_bomb.json"},
-		{"bot_grenadier.json"},
-		{"bot_sniper.json"},
-		{"bot_tactical_missile.json"},
-		{"fabrication_bot.json"},
-		{"fabrication_bot_adv.json"},
-		{"fabrication_bot_combat.json"},
-		{"fabrication_bot_combat_adv.json"},
-		{"fabrication_vehicle.json"},
-		{"fabrication_vehicle_adv.json"},
-		{"land_scout.json"},
-		{"tank_heavy_mortar.json"},
-		{"tank_laser_adv.json"},
-		{"tank_light_laser.json"},
-		{"orbital_fabrication_bot.json"},
-		{"orbital_fighter.json"},
-		{"orbital_lander.json"},
-		{"orbital_laser.json"},
-		{"radar_satellite.json"},
-		{"radar_satellite_adv.json"},
-		{"solar_array.json"},
-		{"battleship.json"},
-		{"destroyer.json"},
-		{"fabrication_ship.json"},
-		{"fabrication_ship_adv.json"},
-		{"frigate.json"},
-		{"missile_ship.json"},
-		{"tank_armor.json", "1.5"},
-		{"tank_heavy_armor.json", "1.5"},
-		{"sea_scout.json"}
-	};
-	
-	private static String[][] damage_change = {
-		{"damage", "2"},
-		{"air_defense_ammo.json"},
-		{"air_defense_adv_ammo.json"},
-		{"artillery_long_ammo.json"},
-		{"artillery_short_ammo.json"},
-		{"laser_defense_ammo.json"},
-		{"laser_defense_adv_ammo.json"},
-		{"tactical_missile_launcher_ammo.json"},
-		{"laser_defense_single_ammo.json"},
-		{"defense_satellite_ammo.json"},
-		{"ion_defense_ammo.json"},
-		{"torpedo_launcher_ammo.json"},
-		{"torpedo_launcher_adv_ammo.json"},
-		{"base_commander_ammo.json"},
-		{"base_commander_aa_ammo.json"},
-		{"base_commander_torpedo_ammo.json"},
-		{"cannon_uber.json"}
-	};
-	
-	private static String[][] splash_damage_change = {
-		{"splash_damage", "2"},
-		{"air_defense_adv_ammo.json"},
-		{"artillery_long_ammo.json"},
-		{"base_commander_aa_ammo.json"},
-		{"artillery_short_ammo.json"},
-		{"cannon_uber.json"}
-	};
-	
-	private static String[][] cfabber_cost_change = {
-		{"build_metal_cost", "0.5"},
-		{"fabrication_bot_combat.json"},
-		{"fabrication_bot_combat_adv.json"}
-	};
-	
-	private static String[][] cfab_range_change = {
-		{"max_range", "2"},
-		{"fabrication_bot_combat_build_arm.json"},
-		{"fabrication_bot_combat_adv_build_arm.json"}
-	};
-	
-	
-	/*
-	 * override type, rather than multiply the value it just outright replaces it
-	 * Format: 
-	 * index 0: {JSON key, "value"},
-	 * index 1+: {"JSON file", "override value"} where override value is optional
-	 */
-	
-	private static String[][] uber_override = {
-		{"damage", "1500"},
-		{"cannon_uber"}
-	};
-	private static String[][] uber_splash_override = {
-		{"splash_damage", "1500"},
-		{"cannon_uber"}
-	};
-	
-	public static String[][][] multiplier_list = {
-		max_health_change,
-		damage_change,
-		splash_damage_change,
-		cfabber_cost_change,
-		cfab_range_change
-	};
-	
-	public static String[][][] absolute_list = {
-		//uber_override,
-		//uber_splash_override
-	};
-	
-	//deprecated
-	public static String[][][] action_list;
-	public static String[][][] path_overrides;
-	public static String[][][] absolute_overrides;
-	public static String basedir;
+	public static void populateLists() {
+		mlist = new ArrayList<ArrayList<ArrayList<String>>>();
+		alist = new ArrayList<ArrayList<ArrayList<String>>>();
+		File infile = new File("ActionLists.txt");
+		String line;
+		try {
+			FileReader infilereader = new FileReader(infile);
+			BufferedReader reader = new BufferedReader(infilereader);
+			while ((line = reader.readLine()) != null) {
+				line = line.trim();
+				System.out.println(line);
+				if (line.startsWith("//") || line.isEmpty() || line.startsWith("}")) {
+					continue;
+				} else if (line.startsWith("srcdir")) {
+					int sindex = line.indexOf("=");
+					srcdir = line.substring(sindex+1).trim();
+				} else if (line.startsWith("moddir")) {
+					int mindex = line.indexOf("=");
+					moddir = line.substring(mindex+1).trim();
+				}
+				else {
+					if (line.startsWith("{")) {
+						ArrayList<String> keyvalue = new ArrayList<String>();
+						String key;
+						String value;
+						int iindex = line.indexOf("\"", 1);
+						int jindex = line.indexOf("\"", iindex+1);
+						key = line.substring(iindex+1, jindex);
+						iindex = line.indexOf("\"", jindex+1);
+						jindex = line.indexOf("\"", iindex+1);
+						value = line.substring(iindex+1, jindex);
+						iindex = line.indexOf("\"", jindex+1);
+						jindex = line.indexOf("\"", iindex+1);
+						curtype = line.substring(iindex+1, jindex);
+						
+						keyvalue.add(key);
+						keyvalue.add(value);
+						ArrayList<ArrayList<String>> array = new ArrayList<ArrayList<String>>();
+						array.add(keyvalue);
+						switch (curtype) {
+						case "multiply":
+							mlist.add(array);
+							//mlist.get(mlist.size()-1).add(keyvalue);
+							break;
+						case "absolute":
+							alist.add(array);
+							//alist.get(alist.size()-1).add(keyvalue);
+							break;
+						}
+					} else {
+						ArrayList<String> filevalue = new ArrayList<String>();
+						String name;
+						int iindex = line.indexOf("\"");
+						int jindex = line.indexOf("\"", iindex+1);
+						name = line.substring(iindex+1, jindex);
+						filevalue.add(name);
+						iindex = line.indexOf("\"", jindex+1);
+						if (iindex != -1) {
+							jindex = line.indexOf("\"", iindex+1);
+							String ovalue = line.substring(iindex+1, jindex);
+							filevalue.add(ovalue);
+						}
+						switch (curtype) {
+						case "multiply":
+							mlist.get(mlist.size()-1).add(filevalue);
+							break;
+						case "absolute":
+							alist.get(alist.size()-1).add(filevalue);
+							break;
+						}
+					}
+					
+				}
+			}
+			reader.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
 }
--- JSONShadow.java
+++ JSONShadow.java
@@ -14,80 +14,41 @@ public class JSONShadow {
 	
 
 	public static void main(String[] args) {
-		if (ActionLists.v1) {
-			executeV1();
-		} else {
+			ActionLists.populateLists();
 			FileTree src = new FileTree(ActionLists.srcdir);
-			//FileTree mod = new FileTree(ActionLists.moddir);
-			for (int i=0; i<ActionLists.multiplier_list.length; i++) {
-				for (int j=1; j<ActionLists.multiplier_list[i].length; j++) {
-					String srcpath = src.search(ActionLists.multiplier_list[i][j][0]).getPath();
+
+			for (int i=0; i<ActionLists.mlist.size(); i++) {
+				for (int j=1; j<ActionLists.mlist.get(i).size(); j++) {
+					String srcpath = src.search(ActionLists.mlist.get(i).get(j).get(0)).getPath();
 					String modext = srcpath.substring(ActionLists.srcdir.length());
 					System.out.print(modext);
 					ValueChanger vc = new ValueChanger(srcpath, ActionLists.moddir + modext);
-					if (ActionLists.multiplier_list[i][j].length == 2) {
-						System.out.println(" | " + ActionLists.multiplier_list[i][0][0] + " * " + ActionLists.multiplier_list[i][j][1]);
-						vc.change(ActionLists.multiplier_list[i][0][0],
-								(int) (Double.parseDouble(ActionLists.multiplier_list[i][j][1])*vc.getValue(ActionLists.multiplier_list[i][0][0])));
+					if (ActionLists.mlist.get(i).get(j).size() == 2) {
+						System.out.println(" | " + ActionLists.mlist.get(i).get(0).get(0) + " * " + ActionLists.mlist.get(i).get(j).get(1));
+						vc.change(ActionLists.mlist.get(i).get(0).get(0),
+								(int) (Double.parseDouble(ActionLists.mlist.get(i).get(j).get(1))*vc.getValue(ActionLists.mlist.get(i).get(0).get(0))));
 					} else {
-						System.out.println(" | " + ActionLists.multiplier_list[i][0][0] + " * " + ActionLists.multiplier_list[i][0][1]);
-						vc.change(ActionLists.multiplier_list[i][0][0],
-								(int) (Double.parseDouble(ActionLists.multiplier_list[i][0][1])*vc.getValue(ActionLists.multiplier_list[i][0][0])));
+						System.out.println(" | " + ActionLists.mlist.get(i).get(0).get(0) + " * " + ActionLists.mlist.get(i).get(0).get(1));
+						vc.change(ActionLists.mlist.get(i).get(0).get(0),
+								(int) (Double.parseDouble(ActionLists.mlist.get(i).get(0).get(1))*vc.getValue(ActionLists.mlist.get(i).get(0).get(0))));
 					
 					}
 				}
 			}
-			for (int i=0; i<ActionLists.absolute_list.length; i++) {
-				for (int j=1; j<ActionLists.absolute_list[i].length; j++) {
-					String srcpath = src.search(ActionLists.absolute_list[i][j][0]).getPath();
+			for (int i=0; i<ActionLists.alist.size(); i++) {
+				for (int j=1; j<ActionLists.alist.get(i).size(); j++) {
+					String srcpath = src.search(ActionLists.alist.get(i).get(j).get(0)).getPath();
 					String modext = srcpath.substring(ActionLists.srcdir.length());
 					System.out.print(modext);
 					ValueChanger vc = new ValueChanger(srcpath, ActionLists.moddir + modext);
-					if (ActionLists.absolute_list[i][j].length == 2) {
-						System.out.println(" | " + ActionLists.absolute_list[i][0][0] + " -> " + ActionLists.absolute_list[i][j][1]);
-						vc.change(ActionLists.absolute_list[i][0][0], ActionLists.absolute_overrides[i][j][1]);
+					if (ActionLists.alist.get(i).get(j).size() == 2) {
+						System.out.println(" | " + ActionLists.alist.get(i).get(0).get(0) + " -> " + ActionLists.alist.get(i).get(j).get(1));
+						vc.change(ActionLists.alist.get(i).get(0).get(0), ActionLists.alist.get(i).get(j).get(1));
 					} else {
-						System.out.println(" | " + ActionLists.absolute_list[i][0][0] + " -> " + ActionLists.absolute_list[i][0][1]);
-						vc.change(ActionLists.absolute_list[i][0][0], ActionLists.absolute_overrides[i][0][1]);
+						System.out.println(" | " + ActionLists.alist.get(i).get(0).get(0) + " -> " + ActionLists.alist.get(i).get(0).get(1));
+						vc.change(ActionLists.alist.get(i).get(0).get(0), ActionLists.alist.get(i).get(0).get(1));
 					}
 				}
 			}
-		}
-			
-		
-	}
-	
-	private static void executeV1() {
-		//Main loop to act on main action list. 
-		for (int k=0; k<ActionLists.action_list.length; k++) {
-			for (int i=1; i<ActionLists.action_list[k].length; i++) {
-				System.out.println(ActionLists.action_list[k][i][1]);
-				ValueChanger vc = new ValueChanger(ActionLists.srcdir + ActionLists.action_list[k][i][0] + ActionLists.action_list[k][i][1],
-						ActionLists.basedir + ActionLists.moddir + ActionLists.action_list[k][i][0] + ActionLists.action_list[k][i][1]);
-				vc.change(ActionLists.action_list[k][0][0],
-						(int) (Double.parseDouble(ActionLists.action_list[k][0][1])*vc.getValue(ActionLists.action_list[k][0][0])));
-			}
-		}
-		
-		for (int k=0; k<ActionLists.path_overrides.length; k++) {
-			for (int i=1; i<ActionLists.path_overrides[k].length; i++) {
-				System.out.println(ActionLists.path_overrides[k][i][2]);
-				ValueChanger vc = new ValueChanger(ActionLists.path_overrides[k][i][0] + 
-						ActionLists.path_overrides[k][i][2],
-						ActionLists.path_overrides[k][i][1] + ActionLists.path_overrides[k][i][2]);
-				vc.change(ActionLists.path_overrides[k][0][0],
-						(int) (Double.parseDouble(ActionLists.path_overrides[k][0][1])*vc.getValue(ActionLists.path_overrides[k][0][0])));
-			}
-		}
-		
-		//Loop for manual overrides for values you don't want to perform a broad stroke to. 
-		for (int k=0; k<ActionLists.absolute_overrides.length; k++) {
-			for (int i=1; i<ActionLists.absolute_overrides[k].length; i++) {
-				System.out.println(ActionLists.absolute_overrides[k][i][1]);
-				ValueChanger vc = new ValueChanger(ActionLists.srcdir + ActionLists.absolute_overrides[k][i][0] + ActionLists.absolute_overrides[k][i][1], 
-						ActionLists.basedir + ActionLists.moddir + ActionLists.absolute_overrides[k][i][0] + ActionLists.absolute_overrides[k][i][1]);
-				vc.change(ActionLists.absolute_overrides[k][0][0], ActionLists.absolute_overrides[k][i][2]);
-			}
-		}
 	}
 }
